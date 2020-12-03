25d87294244afe194d17d6b2648cce7b25a25e02
Tình Nguyễn Văn <tinhnguyenvan91@gmail.com>
2020-12-03 14:53:20 +0700

    update run php artisan:optimize

--- src/routes/console.php
+++ src/routes/console.php
@@ -1,21 +1,6 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Console Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "console" middleware group. Now create something great!
-|
-*/
-
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', 'LoginController@index');
-Route::get('/login', 'LoginController@index');
-Route::post('/auth', 'LoginController@auth');
 
 Route::middleware(['auth.console'])->group(
     function () {
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,21 +1,16 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
+use App\Http\Controllers\Admin\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/install', 'InstallController@index');
 Route::post('/install', 'InstallController@install');
 Route::get('/install/migrate', 'InstallController@migrate');
+
+Route::get('admin', [LoginController::class, 'index'])->name('admin.login');
+Route::get('admin/login', [LoginController::class, 'index'])->name('admin.login');
+Route::post('admin/auth', [LoginController::class, 'auth'])->name('admin.auth');
 
 Route::namespace('Site')->group(
     function () {
