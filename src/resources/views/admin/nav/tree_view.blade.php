fe7f3ba2b00eca8879e7d06dd8babd64528c6d7a
Tình Nguyễn Văn <tinhnguyenvan91@gmail.com>
2020-12-11 15:59:01 +0700

    optimize load menu Menu

--- src/resources/views/admin/nav/tree_view.blade.php
+++
@@ -1,8 +0,0 @@
-@foreach($subCategories as $subCategory)
-    <tr data-node-id="{{$subCategory->id}}" data-node-pid="{{$subCategory->parent_id}}">
-        <td>{{$subCategory->name}}</td>
-    </tr>
-    @if(count($subCategory->subcategory))
-        @include('view_affiliate::admin.category.tree_view',['subCategories' => $subCategory->subcategory])
-    @endif
-@endforeach
