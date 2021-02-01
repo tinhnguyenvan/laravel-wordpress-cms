354fdcdb4f4dac3b05dc1f4afda85f52f010c6c4
tinh.nguyen <tinhnguyenvan91@gmail.com>
2021-02-01 23:33:06 +0700

    add element select group

---
+++ src/resources/views/admin/element/form/select_group.blade.php
@@ -0,0 +1,15 @@
+<select name="{{ $name ?? '' }}" id="{{ $name ?? '' }}" class="form-control" {{ !empty($attr) ? $attr : '' }}>
+    @if (!empty($empty))
+        <option value="">{{ trans('common.select_option') }}</option>
+    @endif
+
+    @if (!empty($data))
+        @foreach ($data as $groupName => $items)
+            <optgroup label="{{ $groupName }}">
+                @foreach ($items as $id => $value)
+                    <option {{ $selected == $id ? 'selected': '' }} value="{{$id}}">{{ $value }}</option>
+                @endforeach
+            </optgroup>
+        @endforeach
+    @endif
+</select>
