c6e7cb7d794325e32c97d7fa15ed5e88ccc48b3a
tinh.nguyen <tinhnguyenvan91@gmail.com>
2021-02-01 12:08:00 +0700

    update home

--- src/app/Http/Controllers/Site/HomeController.php
+++ src/app/Http/Controllers/Site/HomeController.php
@@ -9,8 +9,8 @@ final class HomeController extends SiteController
     public function index()
     {
         $data = [];
-        if (View::exists('layout.default.home.index')) {
-            return view('layout.default.home.index', $this->render($data));
+        if (View::exists($this->layout . '.home.index')) {
+            return view($this->layout . '.home.index', $this->render($data));
         } else {
             return redirect(admin_url())->withErrors('Please login admin select template');
         }
