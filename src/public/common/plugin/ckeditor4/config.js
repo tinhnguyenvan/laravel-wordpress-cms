030f9466896976f7c33669b976213928d596c9e1
tinh.nguyen <tinhnguyenvan91@gmail.com>
2021-03-10 14:44:30 +0700

    update ckeditor

--- src/public/common/plugin/ckeditor4/config.js
+++ src/public/common/plugin/ckeditor4/config.js
@@ -2,8 +2,6 @@
  * @license Copyright (c) 2003-2021, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
  */
-alert(configs.filebrowserUploadUrl);
-
 CKEDITOR.editorConfig = function (config) {
     config.language = 'vi';
     config.width = '100%';
