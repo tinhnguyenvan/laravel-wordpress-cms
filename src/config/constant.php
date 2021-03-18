2304bfa2ec1b7eb891d5cb983ee80bc239a8db46
tinh.nguyen <tinhnguyenvan91@gmail.com>
2021-03-18 23:49:26 +0700

    update theme

--- src/config/constant.php
+++ src/config/constant.php
@@ -173,7 +173,6 @@ return [
     ],
     'DS' => '/',
     'LAYOUT_ADMIN' => 'admin.layouts.app',
-    'LAYOUT' => 'product1',
     'PAGE_NUMBER' => 20,
     'MAX_FILE_UPLOAD' => 2,
     'MAX_FILE_SIZE_UPLOAD' => 1000, // 100px
