3850bd28ea3fbfc41bb620974b35e2622a37e82e
tinh.nguyen <tinhnguyenvan91@gmail.com>
2021-04-02 13:32:33 +0700

    update required

--- src/resources/views/admin/element/form/input.blade.php
+++ src/resources/views/admin/element/form/input.blade.php
@@ -4,12 +4,17 @@
         @if(!empty($class) && $class == 'date-time-picker')
             <i class="fa fa-calendar"></i>
         @endif
+
+        @if(!empty($required))
+            <span class="text-danger">(required)</span>
+        @endif
     </label>
     <div class="controls">
         <div class="input-group">
             <input type="{{ $type ?? 'text' }}"
                    name="{{$name}}"
                    id="{{$name}}"
+                   @if(!empty($required)) required @endif
                    placeholder="{{ $placeholder ?? '' }}"
                    value="{{ old($name, $value) }}"
                    class="form-control {{ $class ?? '' }}"
