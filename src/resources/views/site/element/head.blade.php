eadfe9e8db841de8320a614be27239a1c3236f64
tinh.nguyen <tinhnguyenvan91@gmail.com>
2021-05-03 02:11:17 +0700

    update code script default

--- src/resources/views/site/element/head.blade.php
+++ src/resources/views/site/element/head.blade.php
@@ -2,11 +2,15 @@
 <style>
     {!! !empty($config[$theme.'_css']) ? $config[$theme.'_css'] : ''  !!}
 </style>
-
+<script type="text/javascript">
+    let config = {
+        base_url: "{{ base_url() }}",
+    }
+</script>
 @if(config('services.recaptcha.enable'))
     <script
         src="https://www.google.com/recaptcha/api.js?render={{ config('services.recaptcha.site_key') }}"></script>
-    <script>
+    <script type="text/javascript">
         function onSubmit(token) {
             document.getElementsByClassName("recaptcha")[0].submit();
         }
