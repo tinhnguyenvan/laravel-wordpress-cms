7e5f4203943ab87ccc62a3ed48a3d1bbf396b82d
tinh.nguyen <tinhnguyenvan91@gmail.com>
2021-05-07 18:53:05 +0700

    update seo

--- src/resources/views/admin/layout/head.blade.php
+++ src/resources/views/admin/layout/head.blade.php
@@ -1,5 +1,27 @@
 <head>
-    @include('site.element.head')
+    <base href="./">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <meta name="csrf-token" content="{{ csrf_token() }}"/>
+
+    <title>{{ $title }} | Module</title>
+
+    <link href="{{ asset("console/vendor/vendor.css") }}" rel="stylesheet">
+    <link href="{{ asset("console/css/custom.css") }}" rel="stylesheet">
+    <script type="text/javascript">
+        let configs = {
+            'base_url': '{{ base_url() }}',
+            'admin_url': '{{ base_url('admin') }}',
+            'MAX_FILE_UPLOAD': '{{ @config('constant.MAX_FILE_UPLOAD') }}',
+            'link_media_upload': '{{ admin_url('media/upload?_token='. csrf_token()) }}',
+            'ckeditor': '{{ $config['editor_content'] ?? '' }}',
+        };
+    </script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+    <script src="{{ asset("console/js/popper.min.js") }}" type="text/javascript"></script>
+    <script src="{{ asset("console/js/jquery.min.js") }}" type="text/javascript"></script>
+    <script src="{{ asset("console/js/bootstrap.min.js") }}" type="text/javascript"></script>
 
     @if(!empty($config['editor_content']) && $config['editor_content'] == 'summernote' )
         <link rel="stylesheet" href="{{ asset("common/plugin/summernote-0.8.18/summernote.css") }}">
