dcda2b3028b28d2e4bc36dcfae7f76c309645e8c
tinh.nguyen <tinhnguyenvan91@gmail.com>
2021-06-25 01:06:24 +0700

    remove install package default

--- src/app/Console/Commands/InstallCommand.php
+++ src/app/Console/Commands/InstallCommand.php
@@ -4,7 +4,6 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\Storage;
 
 class InstallCommand extends Command
 {
@@ -37,35 +36,8 @@ class InstallCommand extends Command
      */
     public function handle()
     {
-        $this->pluginDefault();
-
-
-        // migration
         Artisan::call('migrate');
         Artisan::call('db:seed --class=UsersTableSeeder');
         Artisan::call('storage:link');
-    }
-
-    private function pluginDefault()
-    {
-        $listPackage = [
-            'woocommerce' => 'https://github.com/tinhnguyenvan/laravel-wordpress-cms-package-woocommerce.git',
-            'tool' => 'https://github.com/tinhnguyenvan/laravel-wordpress-cms-package-tool.git',
-        ];
-
-        foreach ($listPackage as $name => $urlGit) {
-            try {
-                // clone plugin woocommerce default
-                $pathPackage = base_path().'/packages/tinhphp/'.$name;
-                if (!Storage::exists($pathPackage)) {
-                    shell_exec('git clone '.$urlGit.' '.$pathPackage);
-                    Artisan::call('package_'.$name.':install');
-                } else {
-                    $this->error('- Folder plugin woocommerce exist: '.$pathPackage);
-                }
-            } catch (\Exception $e) {
-                $this->error($e->getMessage());
-            }
-        }
     }
 }
