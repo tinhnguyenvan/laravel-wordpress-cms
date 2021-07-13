6401167305f87c07fb3b28bdf62633c0fd9286aa
tinh.nguyen <tinhnguyenvan91@gmail.com>
2021-07-13 15:02:53 +0700

    fix check code

--- src/resources/views/site/layout/member.blade.php
+++ src/resources/views/site/layout/member.blade.php
@@ -23,7 +23,7 @@
 <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
     <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">
         <span data-feather="user"></span>
-        {{ auth('web')->user()->first_name ?: "Admin" }}
+        {{ auth('web')->user()->first_name ?? "Member" }}
     </a>
     <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse"
             data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false"
