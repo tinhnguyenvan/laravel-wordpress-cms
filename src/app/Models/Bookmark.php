a0778a1676e4b4814f4301b425e5707643578a2c
tinh.nguyen <tinhnguyenvan91@gmail.com>
2021-07-19 11:21:08 +0700

    upgrade manager bookmark

--- src/app/Models/Bookmark.php
+++ src/app/Models/Bookmark.php
@@ -3,47 +3,37 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Bookmark extends Model
 {
-    /**
-     * The database table used by the model.
-     *
-     * @var string
-     */
     protected $table = 'web_bookmarks';
 
-    /**
-     * Attributes that should be mass-assignable.
-     *
-     * @var array
-     */
-    protected $fillable = ['model_type', 'model_id', 'user_id', 'created_at', 'updated_at'];
+    protected $fillable = [
+        'model_type',
+        'model_id',
+        'sub_model_type',
+        'sub_model_id',
+        'user_id',
+        'member_id',
+        'created_at',
+        'updated_at'
+    ];
 
-    /**
-     * The attributes excluded from the model's JSON form.
-     *
-     * @var array
-     */
     protected $hidden = [];
 
-    /**
-     * The attributes that should be casted to native types.
-     *
-     * @var array
-     */
     protected $casts = [];
 
-    public function model()
+    public function model(): BelongsTo
     {
         return $this->belongsTo($this->model_type, 'model_id', 'id');
     }
 
-    /**
-     * The attributes that should be mutated to dates.
-     *
-     * @var array
-     */
+    public function member(): BelongsTo
+    {
+        return $this->belongsTo(Member::class, 'member_id', 'id');
+    }
+
     protected $dates = ['created_at', 'updated_at'];
 
     public function getTitleAttribute()
