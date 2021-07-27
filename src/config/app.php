668b5e0393b7a36325c3aba6569bbfd0fb1cb213
tinh.nguyen <tinhnguyenvan91@gmail.com>
2021-07-27 16:31:39 +0700

    update config

--- src/config/app.php
+++ src/config/app.php
@@ -106,7 +106,7 @@ return [
     |
     */
 
-    'fallback_locale' => env('APP_FALLBACK_LOCALE', 'en'),
+    'fallback_locale' => env('APP_FALLBACK_LOCALE', 'en_US'),
 
     /*
     |--------------------------------------------------------------------------
