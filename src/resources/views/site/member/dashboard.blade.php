a2cddffc6600b32b0489e504f3a52022a7b5816e
tinh.nguyen <tinhnguyenvan91@gmail.com>
2021-08-01 01:20:02 +0700

    load default

--- src/resources/views/site/member/dashboard.blade.php
+++ src/resources/views/site/member/dashboard.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <div class="p-5 mb-4 bg-light rounded-3">
         <div class="container-fluid py-5">
-            <h1 class="display-5 fw-bold">Hi, {{ auth('web')->user()->fullname }}</h1>
+            <h1 class="display-5 fw-bold">Hi, {{ auth('web')->user()->fullname ?? 'Guest' }}</h1>
             <h6 class="col-md-12">
                 Welcome back {{ $config['company_name'] }}.
             </h6>
