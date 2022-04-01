f8433e9309ab657da4a861c628ffa35490f07db7
tinhnguyenvan <tinhnguyenvan91@gmail.com>
2022-04-01 11:49:29 +0700

    update save data facebook tich xanh

--- src/app/Http/Kernel.php
+++ src/app/Http/Kernel.php
@@ -112,6 +112,7 @@ class Kernel extends HttpKernel
         'throttle' => ThrottleRequests::class,
         'verified' => EnsureEmailIsVerified::class,
         'locale' => Localization::class,
+        'error_from_session' => ShareErrorsFromSession::class,
 
         // integrate role: 2020-02-16
         'role' => RoleMiddleware::class,
